9e47d416ec73e985e93442aa793dc9ca1b549a45
exli3141 <exli3141@gmail.com>
2017-11-02 10:26:54 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+  return new Error('引数が入っていません。');
+}
+
+const arg = process.argv[2]
+
+for (let i = 0; i <= arg; i++) {
+  console.log(fib(i));  
+}
